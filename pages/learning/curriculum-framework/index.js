55938907397eee0eab1c4a3509bd16d5a74755f9
NasarLalu10 <112628963+NasarLalu10@users.noreply.github.com>
2022-10-29 23:06:25 +0530

    deploy-check

--- pages/learning/curriculum-framework/index.js
+++ pages/learning/curriculum-framework/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import styles from './style.module.css'
 import Link from 'next/link'
-import Breadcrumb from '../../components/Breadcrumb/Breadcrumb'
+import Breadcrumb from '../../../components/Breadcrumb/Breadcrumb'
 
 function CurriculumFrameWork() {
 
@@ -106,14 +106,14 @@ function CurriculumFrameWork() {
 
                   <nav>
 
-                    <div class="nav nav-tabs whitebg" id="nav-tab" role="tablist">
+                    <div className="nav nav-tabs whitebg" id="nav-tab" role="tablist">
 
-                      <button class="nav-link color-black active text-start" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">
+                      <button className="nav-link color-black active text-start" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">
                         <p className='m-0'>Highlights of</p>
                         <h6>Indoor sports activities</h6>
                       </button>
 
-                      <button class="nav-link color-black text-start" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">
+                      <button className="nav-link color-black text-start" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">
                         <p className='m-0'>Highlights of</p>
                         <h6>Outdoor sports activities</h6>
                       </button>
@@ -122,9 +122,9 @@ function CurriculumFrameWork() {
 
                   </nav>
 
-                  <div class="tab-content" id="nav-tabContent">
+                  <div className="tab-content" id="nav-tabContent">
 
-                    <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">
+                    <div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">
 
                       <ul className='mb-0'>
 
@@ -137,7 +137,7 @@ function CurriculumFrameWork() {
                       </ul>
 
                     </div>
-                    <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">
+                    <div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">
 
                       <ul className='mb-0'>
 
@@ -169,21 +169,21 @@ function CurriculumFrameWork() {
 
                 <nav>
 
-                  <div class="nav nav-tabs whitebg curriculum-tab-1" id="nav-tab" role="tablist">
+                  <div className="nav nav-tabs whitebg curriculum-tab-1" id="nav-tab" role="tablist">
 
-                    <button class="nav-link color-black active text-start" id="nav-dance-tab" data-bs-toggle="tab" data-bs-target="#nav-dance" type="button" role="tab" aria-controls="nav-Dance" aria-selected="true">
+                    <button className="nav-link color-black active text-start" id="nav-dance-tab" data-bs-toggle="tab" data-bs-target="#nav-dance" type="button" role="tab" aria-controls="nav-Dance" aria-selected="true">
                       <h6>Dance</h6>
                     </button>
 
-                    <button class="nav-link color-black text-start " id="nav-music-tab" data-bs-toggle="tab" data-bs-target="#nav-music" type="button" role="tab" aria-controls="nav-Music" aria-selected="false">
+                    <button className="nav-link color-black text-start " id="nav-music-tab" data-bs-toggle="tab" data-bs-target="#nav-music" type="button" role="tab" aria-controls="nav-Music" aria-selected="false">
                       <h6>Music</h6>
                     </button>
 
-                    <button class="nav-link color-black text-start" id="nav-theater-tab" data-bs-toggle="tab" data-bs-target="#nav-theater" type="button" role="tab" aria-controls="nav-theater" aria-selected="false">
+                    <button className="nav-link color-black text-start" id="nav-theater-tab" data-bs-toggle="tab" data-bs-target="#nav-theater" type="button" role="tab" aria-controls="nav-theater" aria-selected="false">
                       <h6>Theater</h6>
                     </button>
 
-                    <button class="nav-link color-black text-start" id="nav-visualArt-tab" data-bs-toggle="tab" data-bs-target="#nav-visualArt" type="button" role="tab" aria-controls="nav-visualArt" aria-selected="false">
+                    <button className="nav-link color-black text-start" id="nav-visualArt-tab" data-bs-toggle="tab" data-bs-target="#nav-visualArt" type="button" role="tab" aria-controls="nav-visualArt" aria-selected="false">
                       <h6>Visual Art</h6>
                     </button>
 
@@ -191,21 +191,21 @@ function CurriculumFrameWork() {
 
                 </nav>
 
-                <div class="tab-content" id="nav-tabContent">
+                <div className="tab-content" id="nav-tabContent">
 
-                  <div class="tab-pane fade show active" id="nav-dance" role="tabpanel" aria-labelledby="nav-dance-tab" tabindex="0">
+                  <div className="tab-pane fade show active" id="nav-dance" role="tabpanel" aria-labelledby="nav-dance-tab" tabindex="0">
                     <p className='pt-3'>The dance programme part of VIKAS MANTRA PUBLIC SCHOOL Co-curricular includes traditional Indian classical dance techniques such as Bharatnatyam , Folk as well as Western dance forms. Dance provides students with an opportunity to find creative expression and cultural exposure. Physical dance lessons are combined with lessons on the history of dance and discussions on the cultural context of the dance form.</p>
                   </div>
 
-                  <div class="tab-pane fade" id="nav-music" role="tabpanel" aria-labelledby="nav-music-tab" tabindex="0">
+                  <div className="tab-pane fade" id="nav-music" role="tabpanel" aria-labelledby="nav-music-tab" tabindex="0">
                     <p className='pt-3'>The dance programme part of VIKAS MANTRA PUBLIC SCHOOL Co-curricular includes traditional Indian classical dance techniques such as Bharatnatyam , Folk as well as Western dance forms. Dance provides students with an opportunity to find creative expression and cultural exposure. Physical dance lessons are combined with lessons on the history of dance and discussions on the cultural context of the dance form.</p>
                   </div>
 
-                  <div class="tab-pane fade" id="nav-theater" role="tabpanel" aria-labelledby="nav-theater-tab" tabindex="0">
+                  <div className="tab-pane fade" id="nav-theater" role="tabpanel" aria-labelledby="nav-theater-tab" tabindex="0">
                     <p className='pt-3'>The dance programme part of VIKAS MANTRA PUBLIC SCHOOL Co-curricular includes traditional Indian classical dance techniques such as Bharatnatyam , Folk as well as Western dance forms. Dance provides students with an opportunity to find creative expression and cultural exposure. Physical dance lessons are combined with lessons on the history of dance and discussions on the cultural context of the dance form.</p>
                   </div>
 
-                  <div class="tab-pane fade" id="nav-visualArt" role="tabpanel" aria-labelledby="nav-visualArt-tab" tabindex="0">
+                  <div className="tab-pane fade" id="nav-visualArt" role="tabpanel" aria-labelledby="nav-visualArt-tab" tabindex="0">
                     <p className='pt-3'>The dance programme part of VIKAS MANTRA PUBLIC SCHOOL Co-curricular includes traditional Indian classical dance techniques such as Bharatnatyam , Folk as well as Western dance forms. Dance provides students with an opportunity to find creative expression and cultural exposure. Physical dance lessons are combined with lessons on the history of dance and discussions on the cultural context of the dance form.</p>
                   </div>
 
@@ -221,17 +221,17 @@ function CurriculumFrameWork() {
 
                 <nav>
 
-                  <div class="nav nav-tabs whitebg curriculum-tab-1" id="nav-tab" role="tablist">
+                  <div className="nav nav-tabs whitebg curriculum-tab-1" id="nav-tab" role="tablist">
 
-                    <button class="nav-link color-black active text-start" id="nav-studentClub-tab" data-bs-toggle="tab" data-bs-target="#nav-studentClub" type="button" role="tab" aria-controls="nav-studentClub" aria-selected="true">
+                    <button className="nav-link color-black active text-start" id="nav-studentClub-tab" data-bs-toggle="tab" data-bs-target="#nav-studentClub" type="button" role="tab" aria-controls="nav-studentClub" aria-selected="true">
                       <h6>Student Clubs</h6>
                     </button>
 
-                    <button class="nav-link color-black text-start" id="nav-communityServices-tab" data-bs-toggle="tab" data-bs-target="#nav-communityServices" type="button" role="tab" aria-controls="nav-communityServices" aria-selected="false">
+                    <button className="nav-link color-black text-start" id="nav-communityServices-tab" data-bs-toggle="tab" data-bs-target="#nav-communityServices" type="button" role="tab" aria-controls="nav-communityServices" aria-selected="false">
                       <h6>Community Service</h6>
                     </button>
 
-                    <button class="nav-link color-black text-start" id="nav-fieldTrip-tab" data-bs-toggle="tab" data-bs-target="#nav-fieldTrip" type="button" role="tab" aria-controls="nav-fieldTrip" aria-selected="false">
+                    <button className="nav-link color-black text-start" id="nav-fieldTrip-tab" data-bs-toggle="tab" data-bs-target="#nav-fieldTrip" type="button" role="tab" aria-controls="nav-fieldTrip" aria-selected="false">
                       <h6>Field Trips and Excursions</h6>
                     </button>
 
@@ -239,29 +239,9 @@ function CurriculumFrameWork() {
 
                 </nav>
 
-                <div class="tab-content" id="nav-tabContent">
+                <div className="tab-content" id="nav-tabContent">
 
-                  <div class="tab-pane fade show active" id="nav-studentClub" role="tabpanel" aria-labelledby="nav-studentClub-tab" tabindex="0">
-
-                    <p className='pt-3'>Students are encouraged to participate in clubs which provide an early exposure to life beyond academics. These clubs provide an avenue for students to explore their personal interests through fun social activities.</p>
-
-                    <h6>Clubs at VIKAS MANTRA PUBLIC SCHOOL</h6>
-
-                    <ul>
-                      <li className='pt-3'>Eco club</li>
-                      <li className='pt-3'>Photography club</li>
-                      <li className='pt-3'>Robotics club</li>
-                      <li className='pt-3'>Cookery club</li>
-                      <li className='pt-3'>Math club</li>
-                      <li className='pt-3'>Literary club</li>
-                      <li className='pt-3'>Theatre club</li>
-                      <li className='pt-3'>Social Welfare club</li>
-                      <li className='pt-3'>Astronomy club</li>
-                    </ul>
-
-                  </div>
-
-                  <div class="tab-pane fade" id="nav-communityServices" role="tabpanel" aria-labelledby="nav-communityServices-tab" tabindex="0">
+                  <div className="tab-pane fade show active" id="nav-studentClub" role="tabpanel" aria-labelledby="nav-studentClub-tab" tabindex="0">
 
                     <p className='pt-3'>Students are encouraged to participate in clubs which provide an early exposure to life beyond academics. These clubs provide an avenue for students to explore their personal interests through fun social activities.</p>
 
@@ -281,7 +261,27 @@ function CurriculumFrameWork() {
 
                   </div>
 
-                  <div class="tab-pane fade" id="nav-fieldTrip" role="tabpanel" aria-labelledby="nav-fieldTrip-tab" tabindex="0">
+                  <div className="tab-pane fade" id="nav-communityServices" role="tabpanel" aria-labelledby="nav-communityServices-tab" tabindex="0">
+
+                    <p className='pt-3'>Students are encouraged to participate in clubs which provide an early exposure to life beyond academics. These clubs provide an avenue for students to explore their personal interests through fun social activities.</p>
+
+                    <h6>Clubs at VIKAS MANTRA PUBLIC SCHOOL</h6>
+
+                    <ul>
+                      <li className='pt-3'>Eco club</li>
+                      <li className='pt-3'>Photography club</li>
+                      <li className='pt-3'>Robotics club</li>
+                      <li className='pt-3'>Cookery club</li>
+                      <li className='pt-3'>Math club</li>
+                      <li className='pt-3'>Literary club</li>
+                      <li className='pt-3'>Theatre club</li>
+                      <li className='pt-3'>Social Welfare club</li>
+                      <li className='pt-3'>Astronomy club</li>
+                    </ul>
+
+                  </div>
+
+                  <div className="tab-pane fade" id="nav-fieldTrip" role="tabpanel" aria-labelledby="nav-fieldTrip-tab" tabindex="0">
 
                     <p className='pt-3'>Students are encouraged to participate in clubs which provide an early exposure to life beyond academics. These clubs provide an avenue for students to explore their personal interests through fun social activities.</p>
 
